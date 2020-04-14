8f72b313818fbbe56ff652658a1a9e0af3767aa2
Robert Fischer <rof263@g.harvard.edu>
2020-04-14 10:31:16 -0500

    Adding Graph support

--- src/mop/azure/plugins/execution.py
+++ src/mop/azure/plugins/execution.py
@@ -1,7 +1,9 @@
 import importlib
 import re
-from mop.framework.mopbase import MopBase
+import os
 
+from mop.azure.utils.create_configuration import change_dir, OPERATIONSPATH
+from mop.framework.mopbase import MopBase
 
 class Execution(MopBase):
 
@@ -17,9 +19,13 @@ class Execution(MopBase):
 
     def run(self, plugin_name):
         plugins = self.config['PLUGINS']
-        plugin_path = self.config['PLUGINS']['path01']
+        root_path = self.config["DEFAULT"]["plugin_root_path"]
+
+        with change_dir(OPERATIONSPATH):
+            path = '{}/{}'.format(os.getcwd(), root_path)
 
         for plugin in plugins:
             if re.match('plugin_', plugin, re.IGNORECASE):
                 self.plugins[plugin] = self.config['PLUGINS'][plugin]
-        return
+
+        return path
